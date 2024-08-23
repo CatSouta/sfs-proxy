2f89e9a2f9a6f89cb57b124acfd1ce896b3ce1e7
Souta <soutacn@gmail.com>
2024-08-23 13:38:52 +0800

    fix: 修复不返回源头部信息

--- app/controller/proxy.js
+++ app/controller/proxy.js
@@ -4,6 +4,7 @@ const { Controller } = require("egg");
 const os = require("os");
 const sharp = require("sharp");
 const Minio = require("minio");
+const crypto = require("crypto");
 
 class ProxyController extends Controller {
   async index() {
@@ -32,23 +33,21 @@ class ProxyController extends Controller {
         ); // 去除 BucketName, Key 并修改 HostId 为代理机器 Hostname
       return;
     }
-
     // 检测该文件是否为图片且有参数需要处理
     if (result.headers["content-type"].startsWith("image/") && imageParams) {
       const check = await ctx.curl(ctx.app.config.proxyLink + uri);
+      // 有参数但无文件
       if (check.status === 404) {
         // 获取宽高参数
         const widthMatch = imageParams.match(/(\d+)w/);
         const heightMatch = imageParams.match(/(\d+)h/);
         const width = widthMatch ? parseInt(widthMatch[1], 10) : null;
         const height = heightMatch ? parseInt(heightMatch[1], 10) : null;
-
         // 宽高处理
         let data = sharp(result.data);
         if (width || height) {
           data = data.resize(width, height);
         }
-
         // 获取格式参数
         const format = imageParams.split(".")[1];
         // 检测需转换的是否为图片格式
@@ -59,13 +58,10 @@ class ProxyController extends Controller {
           };
           return;
         }
-
         // 格式处理
         data = data.toFormat(format);
-
         // 获取处理后的图片缓冲区
         const buffer = await data.toBuffer();
-
         // 储存图片至对象储存，避免下次请求重新处理
         const bucketName = uri.split("/")[0];
         const contentType = `image/${format}`;
@@ -75,19 +71,28 @@ class ProxyController extends Controller {
           buffer,
           contentType
         );
-
+        // Hash 获取文件 Etag
+        const hash = crypto.createHash("md5");
+        hash.update(buffer);
+        const etag = hash.digest("hex");
         // 返回数据
         ctx.set("content-type", contentType);
+        ctx.set("Etag", `"${etag}"`);
+        ctx.set("Last-Modified", new Date().toUTCString());
         ctx.body = buffer;
         return;
       }
-      ctx.set("content-type", check.headers["content-type"]);
+      // 有参数且有文件
+      Object.keys(check.headers).forEach((headerKey) => {
+        ctx.set(headerKey, check.headers[headerKey]);
+      });
       ctx.body = check.data;
       return;
     }
-
     // 无参数处理正常展示
-    ctx.set("content-type", result.headers["content-type"]);
+    Object.keys(result.headers).forEach((headerKey) => {
+      ctx.set(headerKey, result.headers[headerKey]);
+    });
     ctx.body = result.data;
   }
 
@@ -107,6 +112,7 @@ class ProxyController extends Controller {
       "bmp",
       "tiff",
     ];
+    // 允许源文件为 SVG 进行图片处理
     if (type === 0) {
       imageExtensions.push("svg");
     }
@@ -114,6 +120,13 @@ class ProxyController extends Controller {
     return imageExtensions.includes(extension);
   }
 
+  /**
+   * 保存文件至对象储存
+   * @param {string} bucket
+   * @param {string} dest
+   * @param {buffer} file
+   * @param {string} contentType
+   */
   async saveFile(bucket, dest, file, contentType) {
     const { ctx } = this;
     const minioClient = new Minio.Client(ctx.app.config.minio);
