32cad74fcd45ff8f877292983681bff5bd14f3ff
Souta <soutacn@gmail.com>
2024-09-20 01:26:00 +0800

    feat: 添加 favicon

--- config/config.default.js
+++ config/config.default.js
@@ -1,9 +1,11 @@
 /* eslint valid-jsdoc: "off" */
+const path = require('path');
+const fs = require('fs');
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = (appInfo) => {
+module.exports = appInfo => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
@@ -11,16 +13,21 @@ module.exports = (appInfo) => {
   const config = (exports = {});
 
   // 中间件配置
-  config.middleware = ["errorHandler"];
+  config.middleware = ['errorHandler'];
 
   // 安全设置
   config.security = {
     csrf: {
-      enable: false, // 关闭 csrf 检测
-    },
+      enable: false // 关闭 csrf 检测
+    }
+  };
+
+  // Favicon 设置
+  config.siteFile = {
+    '/favicon.ico': fs.readFileSync(path.join(__dirname, '../app/public/favicon.ico'))
   };
 
   return {
-    ...config,
+    ...config
   };
 };
