daebd7f620f531ae172ae2ccb56d5b67c2056242
Souta <soutacn@gmail.com>
2024-10-02 22:12:45 +0800

    chore: 添加环境配置文件模板

---
+++ config/config.template.js
@@ -0,0 +1,13 @@
+// config/config.template.js
+
+// 代理链接
+exports.proxyLink = '';
+
+// MinIO 配置
+exports.minio = {
+  endPoint: '',
+  port: 9000,
+  useSSL: false,
+  accessKey: '',
+  secretKey: ''
+};
